dd805c1a2418aece9f043d63e0411a44fb38bac5
johnjkiran <johnjkiran@gmail.com>
2020-07-18 19:44:39 +0530

    updated

--- session1.py
+++ session1.py
@@ -3,9 +3,9 @@ def myFunc():
     n=len(string)
     arr=[]
     for i in range(n):
-      for j in range(i+1,n+1):
-          a=string[i:j]
-          arr.append(a)
+        for j in range(i+1,n+1):
+            a=string[i:j]
+            arr.append(a)
     print(arr)
 
 def my_Func():
